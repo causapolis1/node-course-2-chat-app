33d4c989fcc967b7609123707b9df6a5fba20849
Matheus Souto <causapolis@outlook.com.br>
2017-07-21 09:41:35 -0300

    emitMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -15,15 +15,13 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('New user');
 
-  socket.emit('newMessage', {
-    from:'John',
-    text: 'hey,thank you!',
-    createAt: 123123
-
-  });
-
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+  io.emit('newMessage', {
+    from: message.from,
+    text: message.text,
+    createAt: new Date().getTime()
+  });
 });
 
 
